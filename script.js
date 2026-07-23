242a0c5f9e25170766471ad3afc94e492520b87b
Utibe <greatestuti@gmail.com>
2026-07-23 16:57:09 +0100

    Last commit

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-let input= document.getElementbyId('input-bar');
+let input= document.getElementById('input-bar');
 let buttons = document.querySelectorAll('button');
 
 let string = '';
-let arr = Array.from('button');
- 
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == '='){
+            // Handle equals button click
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            // Handle clear button click
+            string = '';
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'DEL'){
+            // Handle delete button click
+            string = string.slice(0, -1);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == '%'){
+            // Handle percentage button click
+            string = eval(string) / 100;
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == '/'){
+            // Handle division button click
+            string += '/';
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == '*'){
+            // Handle multiplication button click
+            string += '*';
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == '-'){
+            // Handle subtraction button click
+            string += '-';
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == '+'){
+            // Handle addition button click
+            string += '+';
+            input.value = string;
+        }
+
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
